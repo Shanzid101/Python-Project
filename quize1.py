fc1f4f9d2a27e1e6e76defb296b07897d31ab8e7
Shanzid101 <hossainmdshanzid@gmail.com>
2025-05-27 22:40:16 +0600

    Quize1

---
+++ quize1.py
@@ -0,0 +1,8 @@
+a=input('What is your Country?')
+b=input('What is your City?')
+if a == 'Bangladesh':
+    print('Your Country is Bangladesh')
+elif b == 'Dhaka':
+    print('Your City is Dhaka')
+else:
+    print('You are not from Bangladesh')
