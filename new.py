536ec8ba8217b0ffe4f97df30d786111beb9f43d
Shanzid101 <hossainmdshanzid@gmail.com>
2025-05-27 20:08:16 +0600

    why

---
+++ new.py
@@ -0,0 +1,8 @@
+a=input('What is your Country?')
+b=input('What is your City?')
+if a.lower() == 'bangladesh':
+    print('Your Country is Bangladesh')
+elif b.lower() == 'dhaka':
+    print('Your City is Dhaka')
+else:
+    print('You are not from Bangladesh')
