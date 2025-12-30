a0a99ac07003b8f0cc7ee4f7ed55c2a8c3e0ba18
MD. SHANZID HOSSAIN <mdshanzidhossain6@gmail.com>
2025-12-31 00:23:13 +0600

    Update number_guess.py
    new version

--- number_guess.py
+++ number_guess.py
@@ -1,29 +1,33 @@
 import random
 
-high_range= input("Write the top range:")
-if high_range.isdigit():
-    top_range = int(high_range)
+secret_number = random.randint(1, 10)
+
+print("Welcome to the the Number Guessing Game")
+print("I am thinking of a numbert between 1 and 10. You have 3 chances to guess it.")
+
+guess1 = int(input("Enter your first guess:"))
+if guess1 == secret_number:
+    print("Wow, Correct on the first try! You Won")
+elif guess1 < secret_number:
+    print("Too low. Try a large number.")
 else:
-    print("Please enter a valid number")
-    quit()
+    print("Too high! Try a smaller number.")
 
 
-random_number= random.randint(0,top_range)
-guess =  0
-
-while True:
-    guess += 1
-    user_guess = input("Write your guess number:")
-    if user_guess.isdigit():
-        user_guess = int(user_guess)
+if guess1 != secret_number:
+    guess2 = int(input("Enter your second guess:"))
+    if guess2 == secret_number:
+        print("Well done! You guesses it correctly on your second try.")
+    elif guess2 < secret_number:
+        print("Still too low! Try a larger number.")
     else:
-        print("Please enter a number greater than or equal to 0")
-        quit()
+        print("Still too high! Try a smaller number.")
 
-    if user_guess == random_number:
-        print("You guess the right number!")
-        break
-    else:
-        print("Sorry, you didn't guess the right number, try again!")
-        continue
-print("You guess", guess, "times")
+    if guess2 != secret_number:
+        guess3 = int(input("Enter your final guess:"))
+        if guess3 == secret_number:
+            print("Hurry! You guessed it correctly at the very last time.")
+        else:
+            print(f"Oh no! You lost! The correct number was:{secret_number}")
+
+print("Game Over")
